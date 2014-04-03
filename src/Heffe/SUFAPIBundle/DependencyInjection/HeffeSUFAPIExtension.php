d1718135c2060e1c16300c1099d11f6eb6b9efc6
Heffebaycay <heffebaycay@gmail.com>
2014-04-03 23:12:26 +0100

    Updated HeffeSUFAPIBundle to get the Steam WebAPI key as an external symfony parameter

--- src/Heffe/SUFAPIBundle/DependencyInjection/HeffeSUFAPIExtension.php
+++ src/Heffe/SUFAPIBundle/DependencyInjection/HeffeSUFAPIExtension.php
@@ -22,6 +22,8 @@ class HeffeSUFAPIExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
+        $container->setParameter('heffe_sufapi.webapi_key', $config['webapi_key']);
+
         $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yml');
     }
